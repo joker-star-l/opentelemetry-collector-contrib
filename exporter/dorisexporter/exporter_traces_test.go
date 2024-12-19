b002da91a7b26e8f8e75d3456b3a58c0534fbb19
composer <2789706336@qq.com>
2024-12-20 02:19:42 +0800

    test: retry test

--- exporter/dorisexporter/exporter_traces_test.go
+++ exporter/dorisexporter/exporter_traces_test.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 	"testing"
 	"time"
 
@@ -50,10 +51,12 @@ func TestPushTraceData(t *testing.T) {
 	}
 
 	go func() {
-		http.HandleFunc("/api/otel/otel_traces/_stream_load", func(w http.ResponseWriter, _ *http.Request) {
+		mux := http.NewServeMux()
+		mux.HandleFunc("/api/otel/otel_traces/_stream_load", func(w http.ResponseWriter, _ *http.Request) {
 			w.WriteHeader(http.StatusOK)
 			_, _ = w.Write([]byte(`{"Status":"Success"}`))
 		})
+		server.Handler = mux
 		err = server.ListenAndServe()
 		assert.Equal(t, http.ErrServerClosed, err)
 	}()
@@ -64,6 +67,93 @@ func TestPushTraceData(t *testing.T) {
 		time.Sleep(100 * time.Millisecond)
 	}
 	require.NoError(t, err0)
+
+	_ = server.Shutdown(ctx)
+}
+
+func TestPushTraceDataRetry(t *testing.T) {
+	port, err := findRandomPort()
+	require.NoError(t, err)
+
+	config := createDefaultConfig().(*Config)
+	config.Endpoint = fmt.Sprintf("http://127.0.0.1:%d", port)
+	config.CreateSchema = false
+
+	err = config.Validate()
+	require.NoError(t, err)
+
+	exporter := newTracesExporter(zap.NewNop(), config, componenttest.NewNopTelemetrySettings())
+
+	ctx := context.Background()
+
+	client, err := createDorisHTTPClient(ctx, config, nil, componenttest.NewNopTelemetrySettings())
+	require.NoError(t, err)
+	require.NotNil(t, client)
+
+	exporter.client = client
+
+	defer func() {
+		_ = exporter.shutdown(ctx)
+	}()
+
+	server := &http.Server{
+		ReadTimeout: 3 * time.Second,
+		Addr:        fmt.Sprintf(":%d", port),
+	}
+
+	times := 0
+	go func() {
+		mux := http.NewServeMux()
+		mux.HandleFunc("/api/otel/otel_traces/_stream_load", func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			times++
+			if times < 3 {
+				_, _ = w.Write([]byte(`{"Status":"Fail"}`))
+				return
+			}
+			_, _ = w.Write([]byte(`{"Status":"Success"}`))
+		})
+		server.Handler = mux
+		err = server.ListenAndServe()
+		assert.Equal(t, http.ErrServerClosed, err)
+	}()
+
+	isRetryError := func(err error) bool {
+		if err == nil {
+			return false
+		}
+		return strings.HasPrefix(err.Error(), "failed to push trace data, response:")
+	}
+
+	traces := simpleTraces(10)
+	dataAddress := dataAddress(traces)
+	_, ok := retryMaps[labelTrace].Get(dataAddress)
+	require.False(t, ok)
+
+	err0 := fmt.Errorf("Not Started")
+	for i := 0; !isRetryError(err0) && i < 10; i++ { // until server started
+		err0 = exporter.pushTraceData(ctx, traces)
+		time.Sleep(100 * time.Millisecond)
+	}
+	require.True(t, isRetryError(err0))
+
+	label, ok := retryMaps[labelTrace].Get(dataAddress)
+	require.True(t, ok)
+	require.NotEqual(t, "", label)
+
+	// first retry: fail
+	err0 = exporter.pushTraceData(ctx, traces)
+	require.True(t, isRetryError(err0))
+
+	labelRetry, ok := retryMaps[labelTrace].Get(dataAddress)
+	require.True(t, ok)
+	require.Equal(t, label, labelRetry)
+
+	// second retry: success
+	err0 = exporter.pushTraceData(ctx, traces)
+	require.NoError(t, err0)
+	_, ok = retryMaps[labelTrace].Get(dataAddress)
+	require.False(t, ok)
 
 	_ = server.Shutdown(ctx)
 }
