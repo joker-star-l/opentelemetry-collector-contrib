d1f1f586d106e0faf286a7d46dd7fe2864b6d42a
composer <2789706336@qq.com>
2024-12-27 15:11:48 +0800

    parse args

--- exporter/dorisexporter/loadgenerator/main.go
+++ exporter/dorisexporter/loadgenerator/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 
@@ -11,24 +12,27 @@ import (
 )
 
 type Config struct {
-	Port       int
-	Duration   int
-	Speed      int
-	BatchSize  int
-	BatchDelay int
-	Parallel   int
-	Type       string
+	Port      int
+	Duration  int
+	Speed     int
+	BatchSize int
+	Parallel  int
+	Type      string
+}
+
+func parse(cfg *Config) {
+	flag.IntVar(&cfg.Port, "port", 4317, "The port to send data to")
+	flag.IntVar(&cfg.Duration, "duration", 10, "The duration of the test in seconds")
+	flag.IntVar(&cfg.Speed, "speed", 100, "The speed of the test in items per second")
+	flag.IntVar(&cfg.BatchSize, "batchsize", 100, "The size of each batch")
+	flag.IntVar(&cfg.Parallel, "parallel", 1, "The number of parallel clients")
+	flag.StringVar(&cfg.Type, "type", "log", "The type of data to send (log, trace, metric)")
+	flag.Parse()
 }
 
 func main() {
-	cfg := Config{
-		Port:      4317,
-		Duration:  3,
-		Speed:     1,
-		BatchSize: 1,
-		Parallel:  1,
-		Type:      "metric",
-	}
+	cfg := &Config{}
+	parse(cfg)
 
 	options := testbed.LoadOptions{
 		DataItemsPerSecond: cfg.Speed,
@@ -56,13 +60,13 @@ func main() {
 
 	loadGenerator.Start(options)
 
-	for !loadGenerator.IsReady() {
+	for loadGenerator.DataItemsSent() <= 0 {
 		fmt.Println("Waiting for load generator to be ready...")
-		time.Sleep(10 * time.Millisecond)
+		time.Sleep(1 * time.Second)
 	}
 	fmt.Println("Load generator is ready")
 
-	for i := 0; i < cfg.Duration; i++ {
+	for i := 0; i < cfg.Duration-2; i++ {
 		fmt.Println("Stats:", loadGenerator.GetStats())
 		time.Sleep(1 * time.Second)
 	}
---
+++ exporter/dorisexporter/loadgenerator/run.sh
@@ -0,0 +1,1 @@
+./main -port 4317 -duration 10 -speed 1000 -batchsize 1000 -parallel 1 -type trace
